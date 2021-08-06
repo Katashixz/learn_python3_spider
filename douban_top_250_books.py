4ad89f884e60a3e50ef412882eaf2333c193f906
Katashixz <70137546+Katashixz@users.noreply.github.com>
2021-08-06 19:17:38 +0800

    Update douban_top_250_books.py

--- douban_top_250_books.py
+++ douban_top_250_books.py
@@ -2,13 +2,12 @@ import requests
 from bs4 import BeautifulSoup
 import xlwt
 
-
-def request_douban(url):
 headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
                       'Chrome/88.0.4324.146 Safari/537.36',
     }
 
+def request_douban(url):
     try:
         response = requests.get(url=url, headers=headers)
         if response.status_code == 200:
